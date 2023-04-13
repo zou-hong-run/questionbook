4858f8f69bc62b7acc460721331640db266c48a0
zou-hong-run <zhr19853149156@163.com>
2023-04-13 19:54:16 +0800

    添加动画

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
 <template>
-  <router-view />
+  <transition name="slide-fade">
+    <router-view />
+  </transition>
 </template>
+<style lang="scss">
+.slide-fade {
+  position: fixed;
+  left: 0;
+  right: 0;
+  width: 100%;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  left: 0;
+  top: 0;
+  right: 0;
+  position: absolute;
+  transform: translateY(0);
+  opacity: 0;
+}
+
+.slide-fade-enter-active {
+  transition: all  0.6s  ease-in;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.6s ease-in;
+  transform: translateY(-100%);
+  opacity: 1;
+  z-index: 100;
+}
+</style>
